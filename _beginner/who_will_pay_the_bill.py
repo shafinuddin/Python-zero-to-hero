93c17e5afccf7f865ea8cfd061c2ceced4ed5b71
Shafin Uddin <126351123+shafinuddin@users.noreply.github.com>
2025-05-05 16:46:16 +0600

    Add files via upload

---
+++ _beginner/who_will_pay_the_bill.py
@@ -0,0 +1,7 @@
+import random
+friends=["Shafin","Shahriar","Samiul","Shakib","Shamim"]
+#option 01
+print(random.choice(friends))
+#option 02
+random_index=random.randint(0,5)
+print(friends[random_index])
